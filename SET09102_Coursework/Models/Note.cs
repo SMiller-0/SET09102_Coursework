a227bb659e42e97bd89615c5af22884532fb59cd
Steven Miller <stevenerm88@gmail.com>
2025-03-20 23:58:57 +0000

    Updated to View/Model

---
+++ SET09102_Coursework/Models/Note.cs
@@ -0,0 +1,9 @@
+namespace SET09102_Coursework.Models;
+
+internal class Note
+{
+    public string? Filename { get; set; }
+    public string? Text { get; set; }
+    public DateTime? Date { get; set; }
+}
+
