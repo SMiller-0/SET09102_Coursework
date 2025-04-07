4bcc5550bd03e6aa4dcbe97b937d612780207b61
Alanna Mc <Alanna-Mc@users.noreply.github.com>
2025-04-07 16:32:53 +0100

    Updated MauiProgram.cs

--- SET09102_Coursework/MauiProgram.cs
+++ SET09102_Coursework/MauiProgram.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
 
 namespace SET09102_Coursework;
 
@@ -7,6 +9,16 @@ public static class MauiProgram
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
+
+		var a = Assembly.GetExecutingAssembly();
+		using var stream = a.GetManifestResourceStream("SET09102_Coursework.appsettings.json");
+    
+		var config = new ConfigurationBuilder()
+    		.AddJsonStream(stream)
+    		.Build();
+    
+		builder.Configuration.AddConfiguration(config);
+
 		builder
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
