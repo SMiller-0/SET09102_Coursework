1b2152d3c552890adbfbc409f4b8be7a51a6c245
ClareDeLune <40646529@live.napier.ac.uk>
2025-04-15 10:03:57 +0100

    Added Func. for Issue #11
    Just what it says in the title.
    -Now need to add Comments, Tests, and most of all; fix the database permissions!

---
+++ SET09102_Coursework/Models/AirQData.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SET09102_Coursework.Models;
+
+[Table("airQData")]
+[PrimaryKey(nameof(Id))]
+public class AirQData
+{
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Required]
+    [Column("timestamp")]
+    public string Date { get; set; }
+
+    [Required]
+    [Column("nitrogenLevel")]
+    public string Nitrogen { get; set; }
+
+    [Required]
+    [Column("sulphurLevel")]
+    public string Sulphur { get; set; }
+
+    [Required]
+    [Column("particleMatterSmall")]
+    public string particleMatterSmall { get; set; }
+
+    [Required]
+    [Column("particleMatterBig")]
+    public string particleMatterBig { get; set; }
+
+    [ForeignKey("Reading")]
+    [Column("reading_id")]
+    public int ReadingId { get; set; }
+}
---
+++ SET09102_Coursework/Models/WaterQData.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SET09102_Coursework.Models;
+
+[Table("waterQData")]
+[PrimaryKey(nameof(Id))]
+public class WaterQData
+{
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Required]
+    [Column("timestamp")]
+    public string Date { get; set; }
+
+    [Required]
+    [Column("nitriteLevel")]
+    public string Nitrite { get; set; }
+
+    [Required]
+    [Column("nitrateLevel")]
+    public string Nitrate { get; set; }
+
+    [Required]
+    [Column("phosphate")]
+    public string Phosphate { get; set; }
+
+    [Required]
+    [Column("escherichiaColi")]
+    public string EscherichiaColi { get; set; }
+
+    [Required]
+    [Column("intestinalEnterococci")]
+    public string IntestinalEnterococci { get; set; }
+
+    [ForeignKey("Reading")]
+    [Column("reading_id")]
+    public int ReadingId { get; set; }
+}
