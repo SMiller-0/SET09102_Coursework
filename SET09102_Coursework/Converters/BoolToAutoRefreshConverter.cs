339419f6f6cc3ec9c300cfec4718e5d930234a06
Steven Miller <stevenerm88@gmail.com>
2025-04-24 18:12:22 +0100

    Added XML comments

--- SET09102_Coursework/Converters/BoolToAutoRefreshConverter.cs
+++ SET09102_Coursework/Converters/BoolToAutoRefreshConverter.cs
@@ -2,13 +2,42 @@ using System.Globalization;
 
 namespace SET09102_Coursework.Converters;
 
+/// <summary>
+/// Converts boolean values to auto-refresh status text for UI display.
+/// </summary>
+/// <remarks>
+/// This converter is used in XAML bindings to display the current auto-refresh state
+/// as user-friendly text. It converts true/false values to appropriate action text
+/// indicating whether auto-refresh can be started or stopped.
+/// Implements IValueConverter for use in XAML data binding.
+/// </remarks>
 public class BoolToAutoRefreshConverter : IValueConverter
 {
+    /// <summary>
+    /// Converts a boolean value to its corresponding auto-refresh status text.
+    /// </summary>
+    /// <param name="value">The boolean value representing the current auto-refresh state.</param>
+    /// <param name="targetType">The type of the binding target property.</param>
+    /// <param name="parameter">Optional parameter (not used).</param>
+    /// <param name="culture">The culture to use in the converter.</param>
+    /// <returns>
+    /// "Stop Auto-Refresh" if the value is true (auto-refresh is active),
+    /// "Start Auto-Refresh" if the value is false (auto-refresh is inactive).
+    /// </returns>
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
         return (bool)value ? "Stop Auto-Refresh" : "Start Auto-Refresh";
     }
 
+    /// <summary>
+    /// Converts a string back to a boolean value (not implemented).
+    /// </summary>
+    /// <param name="value">The string value to convert back.</param>
+    /// <param name="targetType">The type to convert to.</param>
+    /// <param name="parameter">Optional parameter (not used).</param>
+    /// <param name="culture">The culture to use in the converter.</param>
+    /// <returns>Nothing, throws NotImplementedException.</returns>
+    /// <exception cref="NotImplementedException">This method is not implemented as conversion is only needed in one direction.</exception>
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
         throw new NotImplementedException();
--- SET09102_Coursework/Services/TimerService.cs
+++ SET09102_Coursework/Services/TimerService.cs
@@ -1,19 +1,40 @@
 namespace SET09102_Coursework.Services;
 
+/// <summary>
+/// Provides timer functionality for periodic task execution in the application.
+/// Implements ITimerService interface and manages a dispatcher timer for async operations.
+/// </summary>
 public class TimerService : ITimerService
 {
     private IDispatcherTimer? _timer;
     private Func<Task>? _callback;
     private bool _isRunning;
 
+    /// <summary>
+    /// Gets a value indicating whether the timer is currently running.
+    /// </summary>
+    /// <value>True if the timer is running; otherwise, false.</value>
     public bool IsRunning => _isRunning;
 
+    /// <summary>
+    /// Initialises a new instance of the TimerService class.
+    /// Creates and configures a dispatcher timer for the current application.
+    /// </summary>
     public TimerService()
     {
         _timer = Application.Current?.Dispatcher?.CreateTimer();
         _timer!.Tick += Timer_Tick;
     }
 
+    /// <summary>
+    /// Starts the timer with the specified interval and callback function.
+    /// </summary>
+    /// <param name="interval">The time interval between timer ticks.</param>
+    /// <param name="callback">The async function to execute on each timer tick.</param>
+    /// <remarks>
+    /// If the timer is null, the method will return without starting.
+    /// The callback function is stored and executed on each timer tick.
+    /// </remarks>
     public void Start(TimeSpan interval, Func<Task> callback)
     {
         if (_timer == null) return;
@@ -24,6 +45,12 @@ public class TimerService : ITimerService
         _isRunning = true;
     }
 
+    /// <summary>
+    /// Stops the timer if it is currently running.
+    /// </summary>
+    /// <remarks>
+    /// If the timer is null, the method will return without performing any action.
+    /// </remarks>
     public void Stop()
     {
         if (_timer == null) return;
@@ -32,6 +59,16 @@ public class TimerService : ITimerService
         _isRunning = false;
     }
 
+    /// <summary>
+    /// Event handler for timer tick events.
+    /// Executes the stored callback function if one exists.
+    /// </summary>
+    /// <param name="sender">The source of the timer event.</param>
+    /// <param name="e">Event arguments.</param>
+    /// <remarks>
+    /// This method is async void as it is an event handler.
+    /// It will await the callback execution if a callback is registered.
+    /// </remarks>
     private async void Timer_Tick(object? sender, EventArgs e)
     {
         if (_callback != null)
@@ -40,6 +77,14 @@ public class TimerService : ITimerService
         }
     }
 
+    /// <summary>
+    /// Disposes of the timer service resources.
+    /// Implements the IDisposable pattern.
+    /// </summary>
+    /// <remarks>
+    /// Unregisters the tick event handler, stops the timer, and releases resources.
+    /// This method should be called when the timer service is no longer needed.
+    /// </remarks>
     public void Dispose()
     {
         if (_timer != null)
