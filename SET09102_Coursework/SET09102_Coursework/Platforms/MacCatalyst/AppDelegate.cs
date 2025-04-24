5406c065844a4b28e94692108c1f3bd7ada6398a
ClareDeLune <40646529@live.napier.ac.uk>
2025-04-24 10:17:34 +0100

    Delete SET09102_Coursework/SET09102_Coursework directory
    Mistake

--- SET09102_Coursework/SET09102_Coursework/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace SET09102_Coursework;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
--- SET09102_Coursework/SET09102_Coursework/Views/AllSensorsPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace SET09102_Coursework.Views;
-
-public partial class AllSensorsPage : ContentPage
-{
-	public AllSensorsPage()
-	{
-		InitializeComponent();
-	}
-}
